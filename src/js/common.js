55dd35917080514285d37d01771aa08ac29717a0
吴江 <2731105140@qq.com>
2024-06-17 02:14:14 +0800

    Updates

--- src/js/common.js
+++
@@ -1,2 +0,0 @@
-export const $ = (el,parent=document)=>parent.querySelector(el)
-export const gets = (el,parent=document)=>[...parent.querySelectorAll(el)]
--- src/js/detail.js
+++
@@ -1,8 +0,0 @@
-import '../css/detail.scss'
-import {$} from './common'
-
-let index = location.search.slice(-1) * 1
-const obj = JSON.parse(localStorage.getItem('data')).items[index]
-
-$('h1').innerHTML = obj.title
-$('img').src = obj.img
